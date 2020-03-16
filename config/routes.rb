c0baa17dde59ca650be5214596aa8744b418f6ac
laarchambault <laarchambault@gmail.com>
2020-03-16 13:23:09 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :genres, :artists, only: [:new, :create, :edit, :update, :show]
+  resources :songs, only: [:new, :create, :edit, :update, :show, :index]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.create(name:"Andrew Peterson", bio: "A passion for creative arts and storytelling...")
+Artist.create(name:"Rend Collective", bio: "A dedicated and whimsical group with a single passion...")
+
+Genre.create(name:"Christian")
+Genre.create(name:"Pop")
+
+Song.create(name:"Hallelujah", artist_id: 1, genre_id: 1)
+Song.create(name:"My Lighthouse", artist_id: 1, genre_id: 1)
